48f098a6d45f995502b1441e84bc8b1ac0735729
Andres Briend <briendandres@gmail.com>
2024-02-24 19:45:02 -0300

    Subscribe

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,12 @@
-const Button = ({ label, iconUrl, backgroundColor, textColor, borderColor }) => {
+const Button = ({label, iconUrl,backgroundColor,textColor,borderColor, fullWidth}) => {
   return (
     <button
       className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
-      ${backgroundColor
+      ${
+        backgroundColor
           ? `${backgroundColor} ${textColor} ${borderColor}`
           : "bg-coral-red text-white border-coral-red"
-        } rounded-full ${fullWidth && "w-full"}`}
+      } rounded-full ${fullWidth && "w-full"}`}
     >
       {label}
 
