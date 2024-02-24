46f5dd830d79aa7398f753e469e42350b57f1b36
Andres Briend <briendandres@gmail.com>
2024-02-24 17:22:47 -0300

    index and nav advance

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
-import{Hero,Footer,CustomerReview,PopularProducts,Services,SpecialOffer,Subscribe,SuperQuality} from './sections';
+import{Hero,Footer,CustomerReviews,PopularProducts,Services,SpecialOffer,Subscribe,SuperQuality} from './sections';
+import Nav from './components/Nav'
 
 const App = () => (
   <main className="relative"> 
-    Nav
+    <Nav/>
     <section className="xl:padding-1 wide:padding-r padding-b">
-      Hero
+      <Hero/>
     </section>
     <section className="padding">
-      PopularProducts
+      <PopularProducts/>
     </section>
     <section className="padding">
-      SuperQuality
+      <SuperQuality/>
     </section>
     <section className="padding-x py-10">
-      Services
+      <Services/>
     </section>
     <section className="padding">
-      SpecialOffer
+      <SpecialOffer/>
     </section>
     <section className="bg-pale-blue padding">
-      CustomerReviews
+      <CustomerReviews/>
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
-      Suscribe
+      <Subscribe/>
     </section>
     <section className="bg-black padding-x padding -t pb-8">
-      Footer
+      <Footer/>
     </section>
   </main>
 )
--- src/sections/index.js
+++ src/sections/index.js
@@ -1,16 +1,16 @@
-import Hero from '/Hero';
-import Footer from '/Footer';
-import CustomerReview from '/CustomerReview';
-import PopularProducts from '/PopularProducts';
-import Services from '/Services';
-import SpecialOffer from '/SpecialOffer';
-import Subscribe from '/Subscribe';
-import SuperQuality from '/SuperQuality';
+import Hero from './Hero';
+import Footer from './Footer';
+import CustomerReviews from './CustomerReviews';
+import PopularProducts from './PopularProducts';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
 
 export {
     Hero,
     Footer,
-    CustomerReview,
+    CustomerReviews,
     PopularProducts,
     Services,
     SpecialOffer,
